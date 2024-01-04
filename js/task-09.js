51e07a465b69de87790f0237738d8d8ee7f9dd89
Ichim99 <140713412+Ichim99@users.noreply.github.com>
2024-01-04 15:29:55 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,15 +1,16 @@
-function changeBackgroundColor() {
-  const body = document.body;
-  const colorSpan = document.querySelector('.color');
-  const randomColor = getRandomHexColor();
-  body.style.backgroundColor = randomColor;
-  colorSpan.textContent = randomColor;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
 }
 
-function getRandomHexColor() {
-return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
-}
-//function getRandomHexColor() {
-  //const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  //return `#${randomColor.padStart(6, '0')}`;
-//}
+document.addEventListener('DOMContentLoaded', function() {
+  const colorSpan = document.querySelector('.color');
+  const changeColorButton = document.querySelector('.change-color');
+
+  changeColorButton.addEventListener('click', function() {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+    colorSpan.textContent = randomColor;
+  });
+});
--- js/task-10.js
+++ js/task-10.js
@@ -1,9 +1,11 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
 }
 
 function createBoxes() {
-  const boxCount = parseInt(document.getElementById('boxCount').value, 10) || 0;
+  
   const boxesContainer = document.getElementById('boxes');
   boxesContainer.innerHTML = '';
 
